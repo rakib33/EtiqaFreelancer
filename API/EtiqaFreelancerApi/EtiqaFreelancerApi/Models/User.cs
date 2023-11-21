f7da13f8f3edc09f590ac30ae076550c4619c316
rakibul islam <islam.rakibul@bjitgroup.com>
2023-11-21 11:01:25 +0600

    [User Model] -> [Creating a User model class for freelancers]

---
+++ API/EtiqaFreelancerApi/EtiqaFreelancerApi/Models/User.cs
@@ -0,0 +1,32 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace EtiqaFreelancerApi.Models
+{
+    public class User
+    {
+        [Key]
+        public int Id { get; set; }
+
+        [Required]
+        [MaxLength(50)]
+        public string? UserName { get; set; }
+
+        [Required]
+        [MaxLength(50)]
+        [EmailAddress(ErrorMessage = "Invalid email address")]
+        public string? Email { get; set; }
+
+        [Required]
+        [MinLength(11)]
+        [MaxLength(13)]
+        [Phone(ErrorMessage = "Invalid phone number")]
+        public string? PhoneNumber { get; set; }
+
+        [Required]
+        public string? SkillSets   { get; set; }
+
+        [Required]
+        public string? Hobby { get; set; }
+
+    }
+}
