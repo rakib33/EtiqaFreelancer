cd2717aa441ffc5e7297df201d0109d7e89843df
rakibul islam <islam.rakibul@bjitgroup.com>
2023-11-21 11:53:52 +0600

    [DBContext]->[Add data context class 'FreelancerContext']

--- API/EtiqaFreelancerApi/EtiqaFreelancerApi/DataContext/FreelancerContext.cs
+++ API/EtiqaFreelancerApi/EtiqaFreelancerApi/DataContext/FreelancerContext.cs
@@ -1,8 +1,23 @@
-﻿using Microsoft.EntityFrameworkCore;
+﻿using EtiqaFreelancerApi.Models;
+using Microsoft.EntityFrameworkCore;
 
 namespace EtiqaFreelancerApi.DataContext
 {
     public class FreelancerContext : DbContext
     {
+        protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
+        {
+            optionsBuilder.UseSqlServer("Data Source=DESKTOP-OC677T4;Initial Catalog=EtiqaFreelancerDB;Integrated Security=True");
+            base.OnConfiguring(optionsBuilder);
+        }
+        public DbSet<User>? Users { get; set; }
+        protected override void OnModelCreating(ModelBuilder modelBuilder)
+        {
+            base.OnModelCreating(modelBuilder);      
+            modelBuilder.Entity<User>(b => {
+                b.HasKey(l => l.Id);
+                b.ToTable("User");
+            });
+        }
     }
 }
